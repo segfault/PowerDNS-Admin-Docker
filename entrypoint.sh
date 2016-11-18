a1df1df1ae5c68adde2855d0f893a91bc3ce9673
DACRepair <DACRepair@gmail.com>
2016-11-18 11:43:20 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ -f $APP_PATH/config.py ];
 then
   python $APP_PATH/setup.py
-  pip install -r requirements.txt
+  pip install -r $APP_PATH/requirements.txt
 fi
 
 cd $APP_PATH && python create_db.py
